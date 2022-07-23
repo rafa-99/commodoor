7cd927d67d68605c972f84108a3db234c8874f20
rafa_99 <raroma09@gmail.com>
2022-07-23 19:36:57 +0000

    Written argument parser

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
+import argparse
+
 from libs.modules import run_module
-from libs.io import string_passwords
+from libs.io import string_passwords, write_to_file
 from modules.modules_manager import ModuleManager
 
 if __name__ == '__main__':
+
+# 	parser = argparse.ArgumentParser()
+# 	parser.add_argument("mode", help="select your mode", choices=['attack', 'decrypt', 'genkeys', 'read'])
+# 	parser.add_argument("-t", "--targets", help="sets the attack targets")
+# 	parser.add_argument("-k", "--key", help="set the key file path")
+# 	parser.add_argument("-i", "--input", help="set the input file path")
+# 	parser.add_argument("-o", "--output", help="sets the output file path")
+# 	args = parser.parse_args()
+#
+# 	match args.mode:
+# 		case 'attack':
+# 			if args.targets:
+# 				print(args.targets)
+# 				if args.output:
+# 					print(args.output)
+# 					if args.key:
+# 						print(args.key)
+# 		case 'decrypt':
+# 			if args.input:
+# 				print(args.input)
+# 				if args.key:
+# 					print(args.key)
+# 		case 'genkeys':
+# 			print(args.mode)
+# 		case 'read':
+# 			if args.input:
+# 				print(args.input)
+#
+# 	print(args)
+
+# -----------------------------------------
+
 	passwords = []
 	manager = ModuleManager()
 
-	# -----------------------------------------
+# -----------------------------------------
+
 	manager.select_target_modules('all')
 	drivers = manager.factory_drivers()
 	for driver in drivers:
