b97e841d6b705bf9b6ee540d623555461b5bbb47
rafa_99 <raroma09@gmail.com>
2022-07-24 14:51:57 +0000

    Parser accepts various targets

--- commodoor.py
+++ commodoor.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("mode", help="select your mode", choices=['attack', 'decrypt', 'genkeys', 'read'])
-	parser.add_argument("-t", "--targets", help="sets the attack targets")
+	parser.add_argument("-t", "--targets", help="sets the attack targets", nargs='+')
 	parser.add_argument("-k", "--key", help="set the key file path")
 	parser.add_argument("-i", "--input", help="set the input file path")
 	parser.add_argument("-o", "--output", help="sets the output file path")
@@ -22,24 +22,24 @@ if __name__ == '__main__':
 				passwords = []
 				manager = ModuleManager()
 				manager.select_target_modules(args.targets)
-
 				drivers = manager.factory_drivers()
+
 				for driver in drivers:
 					run_module(passwords, driver)
 
-					if passwords:
-						stringed_passwords = string_passwords(passwords)
+				if passwords:
+					stringed_passwords = string_passwords(passwords)
 
-						if args.output:
+					if args.output:
 
-							if args.key:
-								encrypt(stringed_passwords, args.output, args.key)
-
-							else:
-								write_to_file(stringed_passwords, args.output)
+						if args.key:
+							encrypt(stringed_passwords, args.output, args.key)
 
 						else:
-							print(stringed_passwords)
+							write_to_file(stringed_passwords, args.output)
+
+					else:
+						print(stringed_passwords)
 
 		case 'decrypt':
 
